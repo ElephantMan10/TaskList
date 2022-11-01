912f0eea6e00707e0e3d717421d3efa3a0ccca3d
Elephantman10 <alex.camatchy@gmail.com>
2022-11-01 16:00:15 +0100

    modif

--- Screen/SignOutScreen.js
+++ Screen/SignOutScreen.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { View, Text, Button } from 'react-native'
+import { TokenContext, UsernameContext } from "../Context/Context"
 
 export default function SignOutScreen ({ navigation, route }) {
     return (
-        <Button title='Sign me out' onPress={() => {navigation.navigate('Home')}} />
+        <Button title='Sign me out' onPress={() => {navigation.navigate('Home'); }} />
     )
 }
