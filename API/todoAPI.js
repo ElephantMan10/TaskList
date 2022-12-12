b7625d75bbd500a5a542846a1a5e7c08f90a5069
linthora <alix.danvy@gmail.com>
2022-12-12 15:20:42 +0100

    feur

--- API/todoAPI.js
+++ API/todoAPI.js
@@ -6,6 +6,35 @@ const SIGN_IN =
 const SIGN_UP =
   'mutation($username:String!, $password:String!){signUp(username:$username, password:$password)}'
 
+const GET_TODO_LISTS =
+  `query($username: String!) {
+    taskLists(where: { owner: { username: $username } }) {
+      id
+      title
+    }
+  }`
+/*
+const CREATE_TODO_LIST =
+  `mutation($title: String!, $username: String!) {
+    createTaskLists(
+      input: {
+        title: $title
+        owner: { connect: { where: { username: $username } } }
+      }
+    ) {
+      taskLists {
+        id
+        title
+        owner {
+          username
+        }
+      }
+    }
+  }` */
+
+//const UPDATE_TODO_LIST =
+//  ``
+
 export function signIn (username, password) {
   return fetch(API_URL, {
     method: 'POST',
@@ -56,6 +85,33 @@ export function signUp (username, password) {
         throw jsonResponse.errors[0]
       }
       return jsonResponse.data.signUp
+    })
+    .catch(error => {
+      throw error
+    })
+}
+
+export function getTodoLists (username) {
+  return fetch(API_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      query: GET_TODO_LISTS,
+      variables: {
+        username: username
+      }
+    })
+  })
+    .then(response => {
+      return response.json()
+    })
+    .then(jsonResponse => {
+      if (jsonResponse.errors != null) {
+        throw jsonResponse.errors[0]
+      }
+      return jsonResponse.data.taskLists
     })
     .catch(error => {
       throw error
