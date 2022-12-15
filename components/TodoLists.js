0a26639b104775d54a3f33ef1b0cacb410cd3616
Elephantman10 <alex.camatchy@gmail.com>
2022-12-15 13:07:18 +0100

    modif vite fait de todolists

---
+++ components/TodoLists.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { View, Text, Button } from "react-native";
+// import { TodoList } from "../components/TodoList";
+
+export default function TodoLists () {
+    return (
+        <Text>Liste des TodoLists</Text>
+    )
+}
